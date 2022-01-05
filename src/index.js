7df49ec009fb81f8d88ca01a8a936382cf866bc5
Jared <jbeckersalax9@aol.com>
2022-01-05 15:29:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,109 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-    e.preventDefault()
+document.addEventListener("DOMContentLoaded", () => {
     fetchQuotes()
-    newQuote()
+    const newQuoteForm = document.getElementById("new-quote-form")
+    newQuoteForm.addEventListener("submit", createNewQuote)
 })
 
 function fetchQuotes(){
-    fetch("http://localhost:3000/quotes?_embed=likes")
+    fetch("http://localhost:3000/quotes")
         .then(response => response.json())
-        .then(data => data.forEach(element => {
-            populateQuotes(element)
+        .then(data => data.forEach(quote => {
+            populateQuotes(quote)
         }))
 }
 
-function populateQuotes(e){
+function populateQuotes(element){
     const quoteCardParent = document.getElementById("quote-list")
+    
     const quoteCard = document.createElement("li")
         quoteCard.className = "quote-card"
         quoteCardParent.append(quoteCard)
-    const blockQuote = document.createElement("blockquote")
+    
+        const blockQuote = document.createElement("blockquote")
         blockQuote.className = "blockquote"
         quoteCard.append(blockQuote)
-    const cardQuote = document.createElement("p")
+    
+        const cardQuote = document.createElement("p")
         cardQuote.className ="mb-0"
-        cardQuote.innerText = e.quote
+        cardQuote.innerText = element.quote
         quoteCard.append(cardQuote)
-    const cardAuthor = document.createElement("footer")
+    
+        const cardAuthor = document.createElement("footer")
         cardAuthor.className = "blockquote-footer"
-        cardAuthor.innerText = e.author
+        cardAuthor.innerText = element.author
         quoteCard.append(cardAuthor)
     const br = document.createElement("br")
     quoteCard.append(br)
+
     const likeButton = document.createElement("button")
         likeButton.className = "btn-success"
         likeButton.innerHTML = `Likes: <span>0</span>`
         quoteCard.append(likeButton)
-    const deleteButton = document.createElement("button")
+        likeButton.addEventListener("click", () =>{
+            let likeObj = {
+                quoteId:parseInt(element.id)
+            }
+            fetch("http://localhost:3000/likes", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body:JSON.stringify(likeObj)
+            })
+            .then(response => response.json())
+        })
+        likeButton.addEventListener("click", (e) =>{
+            fetch("http://localhost:3000/likes")
+                .then(response => response.json())
+                .then(data =>{
+                    e.preventDefault()
+                    console.log(data)
+                    let likeArray = []
+                    if(data.forEach(likeEl => {
+                        likeEl.id === element.id
+                    })){
+                        likeArray.push(likeEl)
+                        console.log(likeEl)
+                    }
+                })    
+        })
+        
+        const deleteButton = document.createElement("button")
         deleteButton.className = "btn-danger"
         deleteButton.innerText = "Delete"
         quoteCard.append(deleteButton)
-}
-
-function newQuote(){
-    const formGroup = document.getElementById("new-quote-form")
-    formGroup.addEventListener("submit", (event) => {
-        event.preventDefault()
-        functionWork(event)
-    })
-}
-
-function functionWork(e){
-    const formControl = document.getElementById("new-quote-form").querySelectorAll(".form-control")
-    let quoteObj = {
-        newQuote:e.target.new_quote.value,
-        author:e.target.author.value
+    deleteButton.addEventListener("click", () => {
+        fetch(`http://localhost:3000/quotes/${element.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(data => quoteCard.remove(data))
     }
-    fetch("http://localhost:3000/quotes", {
-        method: "PATCH",
+    )
+}
+
+function createNewQuote(e){
+   e.preventDefault()
+
+   const quoteGrab = document.getElementById("new-quote")
+   const authorGrab = document.getElementById("author")
+   let quoteObj = {
+       quote:quoteGrab.value,
+       author:authorGrab.value
+   }
+
+   fetch("http://localhost:3000/quotes", {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(quoteObj)
         })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => populateQuotes(data))
+
 }
 
